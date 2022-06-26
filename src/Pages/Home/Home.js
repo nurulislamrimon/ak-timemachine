767dfea05b4833f191185f53275568a0f708b8c2
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-06-26 23:38:58 +0600

    initially done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
     const [tHours, setTHours] = useState([]);
     const [tMinutes, setTMinutes] = useState([]);
     const [payment, setPayment] = useState(0);
+    const project = useRef(0);
     const hourlyRate = useRef(0);
     const totalHours = useRef(0);
     const totalMinutes = useRef(0);
@@ -20,9 +21,11 @@ const Home = () => {
         const isAlreadyInProject = projects.find(p => p === data.projectName)
         if (isAlreadyInProject) {
             setProjects([...projects])
-        } else { setProjects([...projects, data.projectName]) }
+        } else {
+            setProjects([...projects, data.projectName])
+        }
         reset();
-        setPayment(0)
+        setPayment(0);
         hourlyRate.current.value = 0;
     }
     const totalPayment = e => {
@@ -39,20 +42,22 @@ const Home = () => {
     }
 
     return (
-        <div className={`grid ${time.length > 0 && 'grid-cols-3'}`}>
+        <div className={`grid ${time.length > 0 && 'lg:grid-cols-3'}`}>
             <form onSubmit={handleSubmit(onSubmit)} className=' grid gap-5'>
                 {/* project name input */}
                 <div className="form-control w-full max-w-xs mx-auto flex justify-center ">
                     <label htmlFor="projectName" className="label">Project Name</label>
-                    <div className='flex'>
-                        <input type="text" {
+                    <div className='flex border rounded-lg'>
+                        <input ref={project} type="text" {
                             ...register("projectName", {
-                                required: true
+                                // required: true
 
                             })} placeholder="Enter projectName here" className="input w-full" />
-                        <select name="selectProjectName">
-                            {projects.map(p => <option>{p}</option>)}
-                        </select>
+                        {/* {projects.length > 0 &&
+                            <select {...register('sProName')}>
+                                {projects.map((p, index) =>
+                                    <option key={index} value={p}>{p}</option>)}
+                            </select>} */}
                     </div>
 
                     {errors.projectName?.type === 'required' &&
@@ -109,29 +114,30 @@ const Home = () => {
                     {projects?.map((p, index) => <div key={index}>
                         <h1 className='text-2xl mt-5'>Project: {p}</h1>
                         {time.map((t, index) =>
-                            t.projectName === p &&
+                            (t.projectName === p) &&
                             <div key={index}>
-                                <h3 className='text-xl mt-2'> {index + 1}. {t.title} - {t.hours}<small>hours</small>, {t.minutes}<small>minutes</small></h3>
+                                <h3 className='text-xl mt-2'> {t.title} - {t.hours}<small>hours</small>, {t.minutes}<small>minutes</small></h3>
                             </div>)
                         }
                     </div>)}
 
                     <button className='btn mt-5 hover:bg-red-600' onClick={clearAll}>Clear all data</button>
                 </div>}
-            <div className='text-center'>
+            <div>
                 {
                     time.length > 0 &&
                     <div>
-                        <h1 className='text-2xl font-bold'>Total time</h1>
-                        <h3 className='text-xl '><span ref={totalHours} className='text-teal-600 font-bold'>{Math.floor((tHours.reduce((a, b) => parseInt(a) + parseInt(b), 0) + (tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) / 60)))}</span>h</h3>
-                        <h3 className='text-xl '><span ref={totalMinutes} className='text-teal-600 font-bold'> {tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) % 60}</span>min</h3>
+                        <h1 className='text-2xl text-center font-bold'>Grand Total</h1>
+                        <h3 className='text-xl text-center'>Project <span className='text-teal-600 font-bold'>{projects.length}</span>,worked for <span className='text-teal-600 font-bold'>{time.length}</span> times</h3>
+                        <div className="text-center text-2xl">Total time counted: <span ref={totalHours} className='text-teal-600 font-bold'> {Math.floor((tHours.reduce((a, b) => parseInt(a) + parseInt(b), 0) + (tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) / 60)))}</span>h <span ref={totalMinutes} className='text-teal-600 font-bold'> {tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) % 60}</span>min
+                        </div>
 
                         {/* hourlyRate input */}
-                        <div className="form-control w-full max-w-xs mx-auto">
-                            <label htmlFor="hourlyRate" className="label">Payment of an hour</label>
+                        <div className="form-control w-full max-w-xs mx-auto mt-5">
+                            <label htmlFor="hourlyRate" className="label">Payment for an hour</label>
                             <input onChange={totalPayment} ref={hourlyRate} type="number" placeholder="Enter hourly rate here" className="input input-bordered w-full" />
                         </div>
-                        <h1 className='text-3xl'>Your total payment: ${payment?.toFixed(2)}</h1>
+                        <h1 className='text-3xl text-center'>Your total payment: ${payment?.toFixed(2)}</h1>
                     </div>
                 }
             </div>
