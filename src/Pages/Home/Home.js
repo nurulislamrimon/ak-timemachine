8409b83d1c8df2962b146f39913acbd11e54aadf
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-06-28 22:34:01 +0600

    organized code and removed unused lines

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,60 +2,56 @@ import React, { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const Home = () => {
-    const [time, setTime] = useState([]);
     const [projects, setProjects] = useState([]);
-    const [newdt, setnewdt] = useState([]);
-    const [tHours, setTHours] = useState([]);
-    const [tMinutes, setTMinutes] = useState([]);
+    const [projectsNames, setProjectsNames] = useState([]);
     const [payment, setPayment] = useState(0);
-    const project = useRef(0);
     const hourlyRate = useRef(0);
     const totalHours = useRef(0);
     const totalMinutes = useRef(0);
     // form hook
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
+
     const onSubmit = (data) => {
+        data.id = projects.length + 1;
+
         if (!data.projectName) {
             data.projectName = 'No Name';
         }
         if (!data.title) {
             data.title = 'Untitled';
         }
-        setTime([...time, data]);
-        setTHours([...tHours, data.hours]);
-        setTMinutes([...tMinutes, data.minutes]);
-        const isAlreadyInProject = projects.find(p => p === data.projectName)
+        setProjects([...projects, data]);
+        const isAlreadyInProject = projectsNames.find(p => p === data.projectName)
         if (isAlreadyInProject) {
-            setProjects([...projects])
+            setProjectsNames([...projectsNames])
         } else {
-            setProjects([...projects, data.projectName])
+            setProjectsNames([...projectsNames, data.projectName])
         }
         reset();
         setPayment(0);
         hourlyRate.current.value = '';
     }
+
     const totalPayment = e => {
         setPayment(isNaN((parseInt(totalHours.current.innerText) * parseFloat(hourlyRate.current.value)) + (parseFloat(hourlyRate.current.value) / 60) * parseInt(totalMinutes.current.innerText)) ? 0 : (parseInt(totalHours.current.innerText) * parseFloat(hourlyRate.current.value)) + (parseFloat(hourlyRate.current.value) / 60) * parseInt(totalMinutes.current.innerText))
     }
     const clearAll = () => {
         const confirm = window.confirm('Are you sure want to reset all data?');
         if (confirm) {
-            setTime([]);
-            setTHours([]);
-            setTMinutes([]);
             setProjects([]);
+            setProjectsNames([]);
         }
     }
 
     return (
-        <div className={`grid ${time.length > 0 && 'lg:grid-cols-3'}`}>
+        <div className={`grid ${projects.length > 0 && 'lg:grid-cols-3'}`}>
             <form onSubmit={handleSubmit(onSubmit)} className=' grid gap-5'>
                 {/* project name input */}
                 <div className="form-control w-full max-w-xs mx-auto flex justify-center ">
                     <label htmlFor="projectName" className="label">Project Name</label>
                     <div className='flex border rounded-lg'>
-                        <input ref={project} type="text" {
+                        <input type="text" {
                             ...register("projectName", {
                                 // required: true
 
@@ -111,21 +107,23 @@ const Home = () => {
                 <button className='btn w-full max-w-xs mx-auto'>Submit</button>
             </form>
             {
-                time.length > 0 && <div className='text-center'>
+                projects.length > 0 && <div className='text-center'>
                     {/* show statement */}
-                    {projects?.map((p, index) => <div key={index}>
+                    {projectsNames?.map((p, index) => <div key={index}>
                         <h1 className='text-2xl mt-5'>Project: <span className='text-teal-600 font-semibold'>{p} </span>
                             {/* hours calculation for each project */}
                             (<span className='text-teal-600'>{
 
-                                Math.floor((time.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.hours), 0) + (time.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.minutes), 0) / 60)))
+                                Math.floor((projects.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.hours), 0) + (projects.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.minutes), 0) / 60)))
 
-                            }</span>h <span className='text-teal-600'>{
+                            }
+                                {/* minutes calculation for each project */}
+                            </span>h <span className='text-teal-600'>{
 
-                                Math.floor((time.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.minutes), 0) % 60))
+                                Math.floor((projects.filter(t => t.projectName === p).reduce((a, b) => a + parseInt(b.minutes), 0) % 60))
 
                             }</span>min)</h1>
-                        {time.map((t, index) =>
+                        {projects.map((t, index) =>
                             (t.projectName === p) &&
                             <div key={index}>
                                 <h3 className='text-xl mt-2'> {t.title} - {t.hours}<small>hours</small>, {t.minutes}<small>minutes</small></h3>
@@ -137,12 +135,16 @@ const Home = () => {
                 </div>}
             <div>
                 {
-                    time.length > 0 &&
+                    projects.length > 0 &&
                     <div>
                         <h1 className='text-2xl text-center font-bold'>Grand Total</h1>
-                        <h3 className='text-xl text-center'>Project <span className='text-teal-600 font-bold'>{projects.length}</span>,worked for <span className='text-teal-600 font-bold'>{time.length}</span> times</h3>
-                        <div className="text-center text-2xl">Total time counted: <span ref={totalHours} className='text-teal-600 font-bold'> {Math.floor((tHours.reduce((a, b) => parseInt(a) + parseInt(b), 0) + (tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) / 60)))}</span>h <span ref={totalMinutes} className='text-teal-600 font-bold'> {tMinutes.reduce((a, b) => parseInt(a) + parseInt(b), 0) % 60}</span>min
-                        </div>
+                        <h3 className='text-xl text-center'>Project <span className='text-teal-600 font-bold'>{projectsNames.length}</span>,worked for <span className='text-teal-600 font-bold'>{projects.length}</span> times</h3>
+                        <div className="text-center text-2xl">Total time counted: <span ref={totalHours} className='text-teal-600 font-bold'> {
+                            // total hours calculation
+                            Math.floor((projects.reduce((a, b) => a + parseInt(b.hours), 0) + (projects.reduce((a, b) => a + parseInt(b.minutes), 0) / 60)))
+                        }</span>h
+                            <span ref={totalMinutes} className='text-teal-600 font-bold'> {
+                                projects.reduce((a, b) => a + parseInt(b.minutes), 0) % 60}</span>min</div>
 
                         {/* hourlyRate input */}
                         <div className="form-control w-full max-w-xs mx-auto mt-5">
